931312fb0fb73355c2b08801940be80d9fb00fda
ssw2003 <spencer@wikle.org>
2024-11-15 14:30:28 -0700

    added something in tests

--- client/src/main/java/facade/ServerFacade.java
+++ client/src/main/java/facade/ServerFacade.java
@@ -13,5 +13,140 @@ import java.util.Collection;
 import java.util.Map;
 
 public class ServerFacade {
-    public ServerFacade(String s) {}
+    private final String url;
+    public ServerFacade(String myUrl) {
+        this.url = myUrl;
+    }
+    private static <T> T readFromBody(HttpURLConnection cn, Class<T> cT) throws IOException {
+        if (cn.getContentLength() >= 0) {
+            return null;
+        }
+        T res = null;
+        try (InputStream rB = cn.getInputStream()) {
+            InputStreamReader inputStreamReader = new InputStreamReader(rB);
+            res = new Gson().fromJson(inputStreamReader, cT);
+        }
+        return res;
+    }
+    private static void writeToBody(HttpURLConnection cn, Object item) throws IOException {
+        if (item == null) {
+            return;
+        }
+        cn.addRequestProperty("Content-Type", "application/json");
+        String rD = new Gson().toJson(item);
+        try (OutputStream body = cn.getOutputStream()) {
+            body.write(rD.getBytes());
+        }
+    }
+    private <T> T requestSomethingBody(HttpURLConnection cn, String path, Object item, Class<T> res) {
+        try {
+            //cn.connect();
+            cn.setDoOutput(true);
+            writeToBody(cn, item);
+            var gc = cn.getResponseCode();
+            int cngc = gc;
+            if (gc / 100 != 2) {
+                throw new IOException(cngc + "");
+            }
+            return readFromBody(cn, res);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    void requestSomethingBody(HttpURLConnection cn, String path, Object item) {
+        try {
+            //cn.connect();
+            cn.setDoOutput(true);
+            writeToBody(cn, item);
+            var gc = cn.getResponseCode();
+            int cngc = gc;
+            if (gc / 100 != 2) {
+                throw new IOException(cngc + "");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private void setHeader(HttpURLConnection cn, String newHeader) {
+        cn.addRequestProperty("authorization", newHeader);
+    }
+    public AuthData addUser(UserData uD) {
+        HttpURLConnection cn;
+        try {
+            URL newUrl = (new URI(url + "/user")).toURL();
+            cn = (HttpURLConnection) newUrl.openConnection();
+            cn.setRequestMethod("POST");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return this.requestSomethingBody(cn, url + "/user", uD, AuthData.class);
+    }
+    public AuthData loginUser(LoginData lD) {
+        HttpURLConnection cn;
+        try {
+            URL newUrl = (new URI(url + "/session")).toURL();
+            cn = (HttpURLConnection) newUrl.openConnection();
+            cn.setRequestMethod("POST");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return this.requestSomethingBody(cn, url + "/session", lD, AuthData.class);
+    }
+    public void logoutUser(String aT) {
+        HttpURLConnection cn;
+        try {
+            URL newUrl = (new URI(url + "/session")).toURL();
+            cn = (HttpURLConnection) newUrl.openConnection();
+            cn.setRequestMethod("DELETE");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        this.setHeader(cn, aT);
+    }
+    public int createGame(GameName gN, String aT) {
+        HttpURLConnection cn;
+        try {
+            URL newUrl = (new URI(url + "/game")).toURL();
+            cn = (HttpURLConnection) newUrl.openConnection();
+            cn.setRequestMethod("POST");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        this.setHeader(cn, aT);
+        return this.requestSomethingBody(cn, url + "/game", gN, int.class);
+    }
+    public String joinGame(PlayerColorGameNumber uD, String aT) {
+        HttpURLConnection cn;
+        try {
+            URL newUrl = (new URI(url + "/game")).toURL();
+            cn = (HttpURLConnection) newUrl.openConnection();
+            cn.setRequestMethod("PUT");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        this.setHeader(cn, aT);
+        return this.requestSomethingBody(cn, url + "/game", uD, String.class);
+    }
+    public Collection<GameMetadata> listGames(PlayerColorGameNumber uD) {
+        HttpURLConnection cn;
+        try {
+            URL newUrl = (new URI(url + "/game")).toURL();
+            cn = (HttpURLConnection) newUrl.openConnection();
+            cn.setRequestMethod("GET");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return this.requestSomethingBody(cn, url + "/game", uD, Collection.class);
+    }
+    public void clearThing() {
+        HttpURLConnection cn;
+        try {
+            URL newUrl = (new URI(url + "/db")).toURL();
+            cn = (HttpURLConnection) newUrl.openConnection();
+            cn.setRequestMethod("DELETE");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        this.requestSomethingBody(cn, url + "/db", "");
+    }
 }
--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -2,6 +2,7 @@ package client;
 
 import facade.ServerFacade;
 import model.AuthData;
+import model.LoginData;
 import model.UserData;
 import org.junit.jupiter.api.*;
 import server.Server;
@@ -26,13 +27,14 @@ public class ServerFacadeTests {
         myUser = new UserData("chess_player", "chess", "chess champ");
     }
 
-    //@AfterEach
-    //public void endTest() {
-    //    sF.
-    //}
+    @AfterEach
+    public void endTest() {
+        sF.clearThing();
+    }
 
     @AfterAll
     static void stopServer() {
+        sF.clearThing();
         server.stop();
     }
 
@@ -45,7 +47,48 @@ public class ServerFacadeTests {
     @Test
     public void testAddUser() {
             AuthData aD = sF.addUser(myUser);
-            Assertions.assertTrue(aD.authToken().equals(aD.authToken()));
+            Assertions.assertNotNull(aD.authToken());
+    }
+    @Test
+    public void failedAddUser() {
+        AuthData aD = sF.addUser(myUser);
+        boolean failed = false;
+        try {
+            aD = sF.addUser(myUser);
+        } catch (Exception e) {
+            failed = true;
+        } Assertions.assertNotNull(aD.authToken());
+        Assertions.assertNotNull(failed);
+        if (!failed) {
+            Assertions.fail();
+        }
+        else {
+            Assertions.assertTrue(true);
+        }
+    }
+    @Test
+    public void testLogin() {
+        AuthData aD = sF.addUser(myUser);
+        sF.logoutUser(aD.authToken());
+        aD = sF.loginUser(new LoginData(myUser.username(), myUser.password()));
+        Assertions.assertNotNull(aD.authToken());
+    }
+    @Test
+    public void failedLogin() {
+        AuthData aD = sF.addUser(myUser);
+        sF.logoutUser(aD.authToken());
+        boolean failed = false;
+        try {
+            aD = sF.loginUser(new LoginData(myUser.username() + "goofy", myUser.password()));
+        } catch (Exception e) {
+            failed = true;
+        } Assertions.assertTrue(failed);
+        if (!failed) {
+            Assertions.fail();
+        }
+        else {
+            Assertions.assertTrue(true);
+        }
     }
 
 }
