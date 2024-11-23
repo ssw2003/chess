2a7abb9b22f36d655dc7f4244e479f96bdab794c
ssw2003 <spencer@wikle.org>
2024-11-23 12:29:38 -0700

    the folder with the codes were changed

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -376,7 +376,7 @@ whiteKingHasMoved = false;
      * @return the chessboard
      */
     public ChessBoard getBoard() {
-        String pv = "pv";
+        String uw = "uw";
         return theBoard.clone();
     }
     public ChessGame clone() {
