56c30206af7c7e46b4d11ddd950b2fd5ec217451
ssw2003 <spencer@wikle.org>
2025-01-24 23:54:33 -0700

    chess board - commit

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -60,6 +60,9 @@ public class ChessPiece {
         }
         Collection<ChessMove> cM = new ArrayList<>();
         return cM;
+        if (board.getPiece(myPosition).getTeamColor() == ChessGame.TeamColor.WHITE) {
+            return cM;
+        }
     }
     private Collection<ChessMove> pawnMoves(ChessBoard board, ChessPosition myPosition, int cl) {
         Collection<ChessMove> cM = new ArrayList<>();
