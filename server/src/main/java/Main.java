04e39b79e7cc03f64c4251ce7c6fe0f281d8bb9b
ssw2003 <spencer@wikle.org>
2025-03-06 22:43:50 -0700

    commit

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -6,6 +6,7 @@ public class Main {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
         Server sve = new Server();
+        String t = "v";
         sve.run(8080);
     }
 }
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,7 +1,13 @@
 package server;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonNull;
+import com.google.gson.JsonSyntaxException;
 import com.sun.tools.javac.Main;
+import model.*;
 import spark.*;
+
+import java.util.Map;
 
 public class Server {
 
@@ -14,13 +20,59 @@ public class Server {
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
-        //Spark.init("::POST", "/user", doIt());
-
+        Spark.post("/user", this::goThingy);
+        Spark.delete("/db", this::goThatThingy);
+        Spark.get("/game", this::goThisThingy);
+        Spark.post("/session", this::thingy);
+        Spark.delete("/session", this::thatThingy);
+        Spark.put("/game", this::thingyThing);
+        Spark.post("/game", this::thisThingy);
         Spark.awaitInitialization();
         return Spark.port();
     }
-    public void doIt() {
-        System.out.println("Happy is me\n");
+
+    private Object thisThingy(Request request, Response response) {
+        //Create game
+    }
+
+    private Object thingyThing(Request request, Response response) {
+        //Join game
+    }
+
+    private Object thatThingy(Request request, Response response) {
+        //LOGOUT
+    }
+
+    private Object thingy(Request request, Response response) {
+        //LOGIN
+    }
+
+    private Object goThisThingy(Request request, Response response) {
+        //Games listing
+    }
+
+    private Object goThatThingy(Request request, Response response) {
+        //Clear
+    }
+
+    private Object goThingy(Request request, Response response) {
+        String usn;
+        String psw;
+        String eml;
+        var gson = new Gson();
+        try {
+            var json = gson.fromJson(request.body(), UserData.class);
+            usn = json.username();
+            psw = json.password();
+            eml = json.email();
+        } catch (JsonSyntaxException e) {
+            response.status(400);
+            var c = Map.of("message", "Error: bad request");
+            return gson.toJson(c);
+        }
+        var d = Map.of("message", "Error: Bad");
+        response.status(500);
+        return gson.toJson(d);
     }
 
     public void stop() {
