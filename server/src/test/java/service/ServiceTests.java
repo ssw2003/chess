aab95eef02c514f9ea5df9dc2583ad6655d8f792
ssw2003 <spencer@wikle.org>
2025-03-17 19:47:17 -0600

    commit thing a

--- server/src/test/java/service/ServiceTests.java
+++ server/src/test/java/service/ServiceTests.java
@@ -3,6 +3,7 @@ import chess.ChessGame;
 import dataaccess.Service;
 import org.junit.jupiter.api.*;
 import passoff.model.*;
+import passoff.server.TestServerFacade;
 import server.Server;
 import java.net.HttpURLConnection;
 import java.util.Arrays;
@@ -11,4 +12,20 @@ import java.util.Locale;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ServiceTests {
     private static Service svc;
+    @BeforeAll
+    public static void getStarted() {
+        svc = new Service();
+    }
+    @AfterAll
+    static void beDone() {
+        svc = new Service();
+    }
+    @BeforeEach
+    public void startThing() {
+        String s = "s";
+    }
+    @AfterEach
+    public void endThing() {
+        String s = "s";
+    }
 }
