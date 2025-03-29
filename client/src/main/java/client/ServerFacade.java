0076faf95f850fc4d7cab891dc8db724e38e2d12
ssw2003 <spencer@wikle.org>
2025-03-28 18:44:19 -0600

    comt a

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -2,11 +2,15 @@ package client;
 
 import chess.ChessGame;
 import chess.InvalidMoveException;
+import com.google.gson.Gson;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.ResponseCache;
 import java.net.URI;
 import java.net.URL;
+import java.util.HashMap;
 
 public class ServerFacade {
     private String svurl;
@@ -19,8 +23,29 @@ public class ServerFacade {
             HttpURLConnection huck = (HttpURLConnection) place.openConnection();
             huck.setRequestMethod(me);
             huck.setDoOutput(true);
+            wB(huck, p);
+            huck.connect();
+            return getBody(re, huck);
         } catch (Exception e) {
-            //
+            throw new InvalidMoveException();
+        }
+    }
+    private <T> T getBody(Class<T> p, HttpURLConnection huck) {
+        T thisThing = null;
+    }
+    private void thrower(HttpURLConnection huck) throws IOException {
+        var st = huck.getResponseCode();
+        if (st / 100 != 2) {
+            throw new IOException();
+        }
+    }
+    private void wB(HttpURLConnection huck, Object p) throws IOException {
+        if (p != null) {
+            huck.addRequestProperty("Content-Type", "application/json");
+            String rD = new Gson().toJson(p);
+            try (OutputStream os = huck.getOutputStream()) {
+                os.write(rD.getBytes());
+            }
         }
     }
 }
